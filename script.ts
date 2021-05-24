7b31d8d450c6d397ab1bbd866096d4f6442b1260
stambouli <aminestambouli90@yahoo.fr>
2021-05-25 00:00:58 +0200

    using typescript compiler

--- script.ts
+++ script.ts
@@ -2,16 +2,16 @@ const form = document.getElementById('signUp');
 const firstName = document.getElementById('firstName') as HTMLInputElement;
 const age = document.getElementById('age') as HTMLInputElement;
 
-const gift = (ageGift) => (+ageGift + 3);
+const gift = (ageGift: number) => (+ageGift + 3);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (age.value < 18) {
+  if (+age.value < 18) {
     alert('inscription refusée');
   } else {
     alert(`Bienvenue ${firstName.value} 
     vous avez ${age.value} ans
-    Vous aurez un cadeau qaund vous aurez ${gift(age.value)}`);
+    Vous aurez un cadeau qaund vous aurez ${gift(+age.value)}`);
     firstName.value = '';
     age.value = '';
   }
